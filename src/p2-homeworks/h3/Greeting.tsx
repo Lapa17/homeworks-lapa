60df029bb1553a81a34aa4cd3b28c2705178adbb
Pavel Laparevich <laparevichpavel@gmail.com>
2021-10-30 13:32:38 +0300

    h3 is done

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -2,25 +2,27 @@ import React from 'react'
 import s from './Greeting.module.css'
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
+    name: string // need to fix any
+    setNameCallback: (e: any) => void // need to fix any
+    addUser: () => void // need to fix any
+    error: string // need to fix any
+    totalUsers: number // need to fix any
 }
 
 // презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
     {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
 ) => {
-    const inputClass = s.error // need to fix with (?:)
+    const inputClass = !error ? s.normal: s.error // need to fix with (?:)
+    const errorClass = !error ? s.errorHide: s.errorMessage
+    const wrapperClass= !error ? s.wrapper: s.wrapperWithEror
 
     return (
-        <div>
+        <div className={wrapperClass}>
             <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
-            <button onClick={addUser}>add</button>
-            <span>{totalUsers}</span>
+            <span className={errorClass}>{error}</span>
+            <button onClick={addUser} className={s.addBtn}>Add</button>
+            <span className={s.counter}>Total users: {totalUsers}</span>
         </div>
     )
 }
--- src/p2-homeworks/h3/HW3.tsx
+++ src/p2-homeworks/h3/HW3.tsx
@@ -1,18 +1,28 @@
 import React, {useState} from 'react'
+import { v1 } from 'uuid'
 import GreetingContainer from './GreetingContainer'
 
 // types
 export type UserType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    _id: string // need to fix any
+    name: string // need to fix any
 }
 
 // уровень работы с глобальными данными
 function HW3() {
-    const [users, setUsers] = useState<any>([]) // need to fix any
+    const [users, setUsers] = useState<Array<UserType>>([]) // need to fix any
 
-    const addUserCallback = (name: any) => { // need to fix any
-        setUsers([]) // need to fix
+    let newUsers = users
+
+    const addUserCallback = (name: string) => { // need to fix any
+        
+        let newUser:UserType ={
+            _id:v1(),
+            name:name
+        }
+        newUsers = [newUser, ...users]
+        setUsers(newUsers) // need to fix
+        console.log(newUsers)
     }
 
     return (
@@ -21,7 +31,7 @@ function HW3() {
             homeworks 3
 
             {/*should work (должно работать)*/}
-            <GreetingContainer users={users} addUserCallback={addUserCallback}/>
+            <GreetingContainer users={newUsers} addUserCallback={addUserCallback}/>
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
