a38e89a0fc11c7f423da8d48add88258e41b56be
Pavel Laparevich <laparevichpavel@gmail.com>
2021-11-13 12:08:18 +0300

    added styles for menu

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { PATH } from './Routes'
+import s from './Header.module.css'
 
 function Header() {
+
+    
     return (
-        <div>
-            <NavLink to={'/pre-junior'}>PreJunior</NavLink>
-            <NavLink to={'/junior'}>Junior</NavLink>
-            <NavLink to={'/junior-plus'}>JuniorPlus</NavLink>
+        <div className={s.wrapper}>
+            <NavLink className={({ isActive }) =>(isActive ? s.active+' '+ s.link : s.link)} to={PATH.PRE_JUNIOR}>PreJunior</NavLink>
+            <NavLink className={({ isActive }) =>(isActive ? s.active+' '+ s.link : s.link)} to={PATH.JUNIOR}>Junior</NavLink>
+            <NavLink className={({ isActive }) =>(isActive ? s.active+' '+ s.link : s.link)} to={PATH.JUNIOR_PlLUS}>JuniorPlus</NavLink>
+            <div className={s.menuBtn}></div>
+
         </div>
     )
 }
