9df34613c41d1bcda5095f9918ee3ebb895aa5d4
Pavel Laparevich <laparevichpavel@gmail.com>
2021-11-22 18:54:41 +0300

    added style for 404 and done HW6

--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import s from '../../styles/Page404.module.css'
 
 function Error404() {
     return (
-        <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+        <div className={s.mainWrapper}>
+            <div className={s.wrapper}>
+                <div className={s.item}>
+                    <h1>Oops!</h1>
+                    <p>It seems like wrong page</p>
+                </div>
+                <img src="https://public-assets.toggl.com/b/static/404Image-3aea842baa39f40a21036fa29832c57d.svg" />
+
+            </div>
         </div>
     )
 }
