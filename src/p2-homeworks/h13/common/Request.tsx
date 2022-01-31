61d7cae6f31b34771fd5e391ac7c5c0a7f48f4ba
Lapa17 <laparevichpavel@gmail.com>
2022-01-31 15:16:38 +0300

    HW13 main func is done

--- src/p2-homeworks/h13/common/Request.tsx
+++ src/p2-homeworks/h13/common/Request.tsx
@@ -7,17 +7,19 @@ import SuperButton from "../../h4/common/c2-SuperButton/SuperButton";
 export const RequestComponent = () => {
 
     const [check, setCheck] = useState(false)
+    const [reqResult, setReqResult] = useState('')
 
     const sendRequest = () => {
         const body = {success: check}
         axios.default.post(
             'https://neko-cafe-back.herokuapp.com/auth/test', body).then(response => {
+            const { errorText, info } = response.data;
             console.log(response)
-            return response
+            setReqResult(errorText)
         }).catch(error => {
             console.error('There was an error!', error);
-            return error
-        });
+            setReqResult('There was an error!')
+            });
 
     };
 
@@ -26,6 +28,7 @@ export const RequestComponent = () => {
             <h1>HW13</h1>
             <SuperCheckbox checked={check} onChangeChecked={setCheck}/>
             <SuperButton onClick={sendRequest}>Send request</SuperButton>
+            <div>{reqResult}</div>
         </>
 
 
